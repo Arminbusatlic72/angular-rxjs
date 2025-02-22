c24c81df8ebc1a0d185f79c3380e14a005b8a574
Armin <busatar@cronos.be>
2025-02-22 15:39:40 +0100

    Creating Observable ny using from() and of() operators subscribe and display data in the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  data: number = 0;
+  constructor() {
+    const numbers$ = from([1, 2, 3, 4, 5]);
+    numbers$.subscribe((data) => {
+      this.data = data;
+      console.log(data);
+    });
+  }
+}
